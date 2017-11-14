728de2cae13d1cd1c93db2ede854bc8bb5c3a18a
YuanFY <1018654313@qq.com>
2017-11-14 21:16:24 +0800

    factoryBean测试

---
+++ src/test/java/com/yuanfy/study/CarFactoryBean.java
@@ -0,0 +1,41 @@
+package com.yuanfy.study;
+
+import org.springframework.beans.factory.FactoryBean;
+
+public class CarFactoryBean implements FactoryBean<Car> {
+
+	private String carInfo;
+	
+	@Override
+	public Car getObject() throws Exception {
+		Car car = new Car();
+		String[] infos = carInfo.split(",");
+		car.setBrand(infos[0]);
+		car.setMaxSpeed(Integer.valueOf(infos[1]));
+		car.setPrice(Integer.valueOf(infos[2]));
+		return car;	
+	}
+
+	@Override
+	public Class<?> getObjectType() {
+		// TODO Auto-generated method stub
+		return Car.class;
+	}
+
+	@Override
+	public boolean isSingleton() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public String getCarInfo() {
+		return carInfo;
+	}
+
+	public void setCarInfo(String carInfo) {
+		this.carInfo = carInfo;
+	}
+	
+	
+	
+}
---
+++ src/test/java/com/yuanfy/study/FactoryBeanTest.java
@@ -0,0 +1,15 @@
+package com.yuanfy.study;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class FactoryBeanTest {
+	public static void main(String[] args) {
+		ApplicationContext bf = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Car car1 = (Car)bf.getBean("car");
+		System.out.println(car1.getBrand());
+		Car car2 = (Car)bf.getBean("&car");
+		System.out.println(car2.getBrand());
+	}
+}
