9245f527da1cb64b87ed547c4efed0927d22a15c
yuanfy <YuanFY@users.noreply.github.com>
2017-12-19 19:56:25 +0800

    优化snmp测试类

--- src/test/java/com/yuanfy/study/snmp/SnmpTrapHandler.java
+++ src/test/java/com/yuanfy/study/snmp/SnmpTrapHandler.java
@@ -8,13 +8,17 @@ import org.snmp4j.CommandResponder;
 import org.snmp4j.CommandResponderEvent;
 import org.snmp4j.MessageDispatcher;
 import org.snmp4j.MessageDispatcherImpl;
+import org.snmp4j.PDU;
 import org.snmp4j.Snmp;
 import org.snmp4j.TransportMapping;
 import org.snmp4j.mp.MPv1;
 import org.snmp4j.mp.MPv2c;
 import org.snmp4j.mp.MPv3;
+import org.snmp4j.security.AuthSHA;
+import org.snmp4j.security.PrivAES128;
 import org.snmp4j.security.SecurityProtocols;
 import org.snmp4j.security.USM;
+import org.snmp4j.security.UsmUser;
 import org.snmp4j.smi.GenericAddress;
 import org.snmp4j.smi.OctetString;
 import org.snmp4j.smi.UdpAddress;
@@ -25,9 +29,9 @@ import org.snmp4j.util.ThreadPool;
 public class SnmpTrapHandler implements CommandResponder{
 	private static Logger log = LoggerFactory.getLogger(SnmpTrapHandler.class);
 	private static int threadNum = 200;
-	private static String ipAddress = "udp:127.0.0.1/";
+	private static String ipAddress = "udp:10.10.112.177/162";
 	private Snmp snmp = null;
-	public void init(int port){
+	public void init(){
 		//1、初始化多线程消息转发类
 		ThreadPool threadPool = ThreadPool.create("SnmpTrap", threadNum);
 		MessageDispatcher messageDispatcher = new MultiThreadedMessageDispatcher(threadPool, new MessageDispatcherImpl()); 
@@ -36,12 +40,14 @@ public class SnmpTrapHandler implements CommandResponder{
 		messageDispatcher.addMessageProcessingModel(new MPv2c());
 		OctetString localEngineID = new OctetString(MPv3.createLocalEngineID());
 		USM usm = new USM(SecurityProtocols.getInstance().addDefaultProtocols(), localEngineID, 0);
-		//usm.addUser(user);
+		UsmUser user = new UsmUser(new OctetString("SNMPV3"), AuthSHA.ID, new OctetString("authPassword"), 
+            PrivAES128.ID, new OctetString("privPassword"));
+        usm.addUser(user.getSecurityName(), user);
 		messageDispatcher.addMessageProcessingModel(new MPv3(usm));
 		//2、创建transportMapping
-		TransportMapping transportMapping = null;
+		TransportMapping<?> transportMapping = null;
 		try {
-			UdpAddress updAddr = (UdpAddress) GenericAddress.parse(System.getProperty("snmp4j.listenAddress", "udp:127.0.0.1/162"));
+			UdpAddress updAddr = (UdpAddress) GenericAddress.parse(System.getProperty("snmp4j.listenAddress", ipAddress));
 			transportMapping = new DefaultUdpTransportMapping(updAddr);
 			//3、正式创建snmp
 			snmp = new Snmp(messageDispatcher, transportMapping);
@@ -54,13 +60,30 @@ public class SnmpTrapHandler implements CommandResponder{
 	}
 	
 	public void start() {
-		init(162);
+		init();
+		//一定要将当前对象添加至commandResponderListeners中
 		snmp.addCommandResponder(this);
-		System.out.println("开始监听trap信息");
+		System.out.println("开始监听trap信息：");
 	}
+	/**
+	 * 处理信息方法
+	 */
 	@Override
 	public void processPdu(CommandResponderEvent event) {
-		System.out.println(event);
+	    String version = null ;
+	    String community = null;
+	    if (event.getPDU().getType() == PDU.V1TRAP) {
+	        version = "v1";
+	        community = new String(event.getSecurityName());
+	    } else if (event.getPDU().getType() == PDU.TRAP){
+	        if (event.getSecurityModel() == 2) {
+	            version = "v2";
+	            community = new String(event.getSecurityName());
+	        }else {
+	            version = "v3";
+	        }
+	    }
+	    System.out.println("接收到的trap信息：[发送来源="+event.getPeerAddress()+",snmp版本="+version+",团体名="+community+", 携带的变量="+event.getPDU().getVariableBindings()+"]");
 	}
 	public static void main(String[] args) {
 		SnmpTrapHandler handler = new SnmpTrapHandler();
--- src/test/java/com/yuanfy/study/snmp/SnmpUtil.java
+++ src/test/java/com/yuanfy/study/snmp/SnmpUtil.java
@@ -1,28 +1,39 @@
 package com.yuanfy.study.snmp;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.Vector;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.snmp4j.CommunityTarget;
+import org.snmp4j.MessageDispatcher;
+import org.snmp4j.MessageDispatcherImpl;
 import org.snmp4j.PDU;
+import org.snmp4j.PDUv1;
+import org.snmp4j.ScopedPDU;
 import org.snmp4j.Snmp;
 import org.snmp4j.Target;
 import org.snmp4j.TransportMapping;
 import org.snmp4j.UserTarget;
 import org.snmp4j.event.ResponseEvent;
+import org.snmp4j.mp.MPv1;
+import org.snmp4j.mp.MPv2c;
+import org.snmp4j.mp.MPv3;
 import org.snmp4j.mp.SnmpConstants;
+import org.snmp4j.security.AuthSHA;
+import org.snmp4j.security.PrivAES128;
 import org.snmp4j.security.SecurityLevel;
 import org.snmp4j.security.SecurityModel;
+import org.snmp4j.security.SecurityProtocols;
+import org.snmp4j.security.USM;
+import org.snmp4j.security.UsmUser;
 import org.snmp4j.smi.GenericAddress;
 import org.snmp4j.smi.OID;
 import org.snmp4j.smi.OctetString;
+import org.snmp4j.smi.UdpAddress;
+import org.snmp4j.smi.Variable;
 import org.snmp4j.smi.VariableBinding;
 import org.snmp4j.transport.DefaultUdpTransportMapping;
-import org.snmp4j.util.DefaultPDUFactory;
-import org.snmp4j.util.TableEvent;
-import org.snmp4j.util.TableUtils;
 
 /**
  * @description snmp工具类
@@ -34,27 +45,39 @@ public class SnmpUtil {
 	private static Logger log = LoggerFactory.getLogger(SnmpUtil.class);
 	public static Snmp snmp = null;
 	private static String community = "public";
-	private static String ipAddress = "udp:127.0.0.1/";
+	private static String ipAddress = "udp:192.168.208.50/";
 	
 	/**
 	 * @description 初始化snmp
 	 * @author YuanFY
 	 * @date 2017年12月16日 上午10:28:01
 	 * @version 1.0
+	 * @throws IOException 
 	 */
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public static void initSnmp(int port){
-        try {
-        	TransportMapping transport = new DefaultUdpTransportMapping();  
-            snmp = new Snmp(transport);  
-		} catch (IOException e) {
-			e.printStackTrace();
-		}  
+	public static void initSnmp() throws IOException{
+            //1、初始化多线程消息转发类
+            MessageDispatcher messageDispatcher = new MessageDispatcherImpl(); 
+            //其中要增加三种处理模型。如果snmp初始化使用的是Snmp(TransportMapping<? extends Address> transportMapping) ,就不需要增加 
+            messageDispatcher.addMessageProcessingModel(new MPv1());
+            messageDispatcher.addMessageProcessingModel(new MPv2c());
+            //当要支持snmpV3版本时，需要配置user
+            OctetString localEngineID = new OctetString(MPv3.createLocalEngineID());
+            USM usm = new USM(SecurityProtocols.getInstance().addDefaultProtocols(), localEngineID, 0);
+            UsmUser user = new UsmUser(new OctetString("SNMPV3"), AuthSHA.ID, new OctetString("authPassword"), 
+                PrivAES128.ID, new OctetString("privPassword"));
+            usm.addUser(user.getSecurityName(), user);
+            messageDispatcher.addMessageProcessingModel(new MPv3(usm));
+            //2、创建transportMapping
+            UdpAddress updAddr = (UdpAddress) GenericAddress.parse("udp:10.10.112.177/161");
+            TransportMapping<?> transportMapping = new DefaultUdpTransportMapping(updAddr);
+            //3、正式创建snmp
+            snmp = new Snmp(messageDispatcher, transportMapping);
+            //开启监听
+            snmp.listen();
 	}
+	
 	/**
 	 * @description 针对snmp版本创建对应的目标对象
-	 * @param version
-	 * @return
 	 * @author YuanFY
 	 * @date 2017年12月16日 下午1:59:22
 	 * @version 1.0
@@ -67,10 +90,11 @@ public class SnmpUtil {
 		}
 		if (version == SnmpConstants.version3) {
 			target = new UserTarget();
+			//snmpV3需要设置安全级别和安全名称，其中安全名称是创建snmp指定user设置的new OctetString("SNMPV3")
 			target.setSecurityLevel(SecurityLevel.AUTH_PRIV);
-			target.setSecurityModel(SecurityModel.SECURITY_MODEL_USM);
-			target.setSecurityName(new OctetString("MD5DES"));
+			target.setSecurityName(new OctetString("SNMPV3"));
 		} else {
+		    //snmpV1和snmpV2需要指定团体名名称
 			target = new CommunityTarget();
 			((CommunityTarget)target).setCommunity(new OctetString(community));
 			if (version == SnmpConstants.version2c) {
@@ -78,10 +102,30 @@ public class SnmpUtil {
 			}
 		}
 		target.setVersion(version);
-		target.setAddress(GenericAddress.parse(ipAddress + port));
+		//必须指定，没有设置就会报错。
+		target.setAddress(GenericAddress.parse(ipAddress+port));
 		target.setRetries(5);
 		target.setTimeout(3000);
 		return target;
+	}
+	/**
+	 * @Description: 创建报文
+	 * @return
+	 * @author yuanfy
+	 * @date 2017年12月19日 上午11:48:04 
+	 * @version 6.5
+	 */
+	private static PDU createPDU(int version, int type, String oid){
+	    PDU pdu = null;
+	    if (version == SnmpConstants.version3) {
+	        pdu = new ScopedPDU();
+	    }else {
+	        pdu = new PDUv1();
+	    }
+	    pdu.setType(type);
+	    //可以添加多个变量oid
+	    pdu.add(new VariableBinding(new OID(oid)));
+	    return pdu;
 	}
 	/**
 	 * @description 通过oid获取节点信息
@@ -90,34 +134,77 @@ public class SnmpUtil {
 	 * @version 1.0
 	 */
 	public static void snmpGet(String oid){
-		Target target = createTarget(SnmpConstants.version1, 161);
-		PDU pdu = new PDU();
-		pdu.add(new VariableBinding(new OID(oid)));
-		pdu.setType(PDU.GET);
-		System.out.println("-------> 发送PDU <-------"); 
-		try {
-			ResponseEvent responseEvent = snmp.send(pdu, target);
-			PDU response = responseEvent.getResponse();
-			System.out.println(response);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	    try {
+	        //1、初始化snmp,并开启监听
+            initSnmp();
+            //2、创建目标对象
+            Target target = createTarget(SnmpConstants.version2c, SnmpConstants.DEFAULT_COMMAND_RESPONDER_PORT);
+            //3、创建报文
+            PDU pdu = createPDU(SnmpConstants.version2c, PDU.GET, oid);
+            System.out.println("-------> 发送PDU <-------"); 
+            //4、发送报文，并获取返回结果
+            ResponseEvent responseEvent = snmp.send(pdu, target);
+            PDU response = responseEvent.getResponse();
+            System.out.println("返回结果：" + response);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+	/**
+	 * @Description: snmpWalk基于snmpGet实现
+	 * @param columnOids
+	 * @author yuanfy
+	 * @date 2017年12月19日 下午4:48:48 
+	 * @version 6.5
+	 */
+	public static void snmpWalk(String oid) {
+        try {
+            //1、初始化snmp,并开启监听
+            initSnmp();
+            //2、创建目标对象
+            Target target = createTarget(SnmpConstants.version2c, SnmpConstants.DEFAULT_COMMAND_RESPONDER_PORT);
+            //3、创建报文
+            PDU pdu = createPDU(SnmpConstants.version2c, PDU.GETNEXT, oid);
+            System.out.println("-------> 发送PDU <-------"); 
+            //4、发送报文，并获取返回结果
+            boolean matched = true;
+            while (matched) {
+                ResponseEvent responseEvent = snmp.send(pdu, target);
+                if (responseEvent == null || responseEvent.getResponse() == null) {
+                    break;
+                }
+                PDU response = responseEvent.getResponse();
+                String nextOid = null;
+                Vector<? extends VariableBinding> variableBindings = response.getVariableBindings();
+                for (int i = 0; i < variableBindings.size(); i++) {
+                    VariableBinding variableBinding = variableBindings.elementAt(i);
+                    Variable variable = variableBinding.getVariable();
+                    nextOid = variableBinding.getOid().toDottedString();
+                    //如果不是这个节点下的oid则终止遍历，否则会输出很多，直到整个遍历完。
+                    if (!nextOid.startsWith(oid)) {
+                        matched = false;
+                        break;
+                    }
+                    //System.out.println(variable);
+                }
+                if (!matched) {
+                    break;
+                }
+                pdu.clear();
+                pdu.add(new VariableBinding(new OID(nextOid)));
+                System.out.println("返回结果：" + response);
+            }
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 	}
 	
-	@SuppressWarnings("unused")
-	public static void snmpWalk(String oid) {
-		TableUtils tableUtils = new TableUtils(snmp, new DefaultPDUFactory(PDU.GETBULK));
-		tableUtils.setMaxNumRowsPerPDU(5);
-		OID[] columnOids = new OID[] {  
-                new OID("1.3.6.1.2.1.1.9.1.2"), //sysORID  
-                new OID("1.3.6.1.2.1.1.9.1.3"), //sysORDescr  
-                new OID("1.3.6.1.2.1.1.9.1.5")  //wrong OID, expect an null in in VariableBinding array  
-        };  
-		List<TableEvent> result = tableUtils.getTable(createTarget(SnmpConstants.version2c, 161), columnOids, new OID("3"), new OID("10"));
-	}
 	public static void sendTrap() throws IOException{
-		Target target = createTarget(SnmpConstants.version2c, 162); 
-		PDU pdu = new PDU();  
+		Target target = createTarget(SnmpConstants.version3, 162); 
+		PDU pdu = new ScopedPDU();
         pdu.add(new VariableBinding(new OID(".1.3.6.1.2.3377.10.1.1.1.1"),  
                 new OctetString("SnmpTrap")));  
         pdu.add(new VariableBinding(new OID(".1.3.6.1.2.3377.10.1.1.1.2"),  
@@ -132,4 +219,8 @@ public class SnmpUtil {
 		}	
         
 	}
+	public static void main(String[] args) {
+	    //snmpGet("1.3.6.1.2.1.1.1.0");
+	    snmpWalk("1.3.6.1.2.1.25.3.3.1.2");//CPU的当前负载，N个核就有N个负载
+    }
 }
