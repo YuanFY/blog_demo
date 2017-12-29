004b152f23d1debf71ae0f78a5e914572129aa42
YuanFY <1018654313@qq.com>
2017-12-29 17:26:50 +0800

    增加工具类

---
+++ src/main/java/com/yuanfy/monitorsite/common/util/file/ResPathUtils.java
@@ -0,0 +1,126 @@
+package com.yuanfy.monitorsite.common.util.file;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ResourceBundle;
+
+import org.apache.log4j.Logger;
+
+import com.yuanfy.monitorsite.common.util.StringUtils;
+
+/**
+ * @Description: 系统路径工具类
+ * @author yuanfy
+ * @date 2017年12月28日 下午8:01:00 
+ * @version 1.0
+ */
+public class ResPathUtils {
+    private static Logger log = Logger.getLogger(ResPathUtils.class);
+    
+    public static String osType = System.getProperty("os.name");
+    
+    public static String fileSeparator = System.getProperty("file.separator");
+    
+    private static ResourceBundle resource = ResourceBundle.getBundle("path");
+    
+    /**
+     * @Description: 获取所有额外存放文件的根路径
+     * @return
+     * @author yuanfy
+     * @date 2017年12月29日 下午3:34:04 
+     * @version 6.5
+     */
+    public static String getHome() {
+        String userHome = System.getProperty("user.home");
+        String path = null;
+        if (isWindows()) {
+            path = userHome + (userHome.endsWith(fileSeparator) ? "" : fileSeparator) + ".blog" +fileSeparator;
+        }else {
+            path = getPathByKey("root_path");
+        }
+        createPath(path);
+        return path;
+    }
+    
+    /**
+     * @Description: 获取密钥存放的路径
+     * @return
+     * @author yuanfy
+     * @date 2017年12月29日 下午3:42:17 
+     * @version 6.5
+     */
+    public static String getKeyStorePath(){
+        String path = getHome() + getPathByKey("keystore_path");
+        createPath(path);
+        return path;
+    }
+    
+    /**
+     * @Description: 获取license文件存放的路径
+     * @return
+     * @author yuanfy
+     * @date 2017年12月29日 下午3:42:17 
+     * @version 6.5
+     */
+    public static String getLicensePath(){
+        String path = getHome() + getPathByKey("license_file_path");
+        createPath(path);
+        return path;
+    }
+    
+    /**
+     * @Description: 判断系统是否为操作系统
+     * @return
+     * @author yuanfy
+     * @date 2017年12月29日 下午3:43:28 
+     * @version 6.5
+     */
+    public static boolean isWindows() {
+        if (StringUtils.isEmpty(osType) || osType.toLowerCase().contains("windows")) {
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * @Description: 根据path.properties中的key获取其对应的值。
+     * @author yuanfy
+     * @date 2017年12月29日 下午3:44:36 
+     * @version 6.5
+     */
+    public static String getPathByKey(String key) {
+        return resource.getString(key);
+    }
+    
+    /**
+     * @Description: 根据路径创建（如果路径不存在，则创建路径）
+     * @param path 路径
+     * @param isDir 是否为目录
+     * @return
+     * @author yuanfy
+     * @date 2017年12月29日 下午4:07:32 
+     * @version 6.5
+     */
+    public static File createPath(String path, boolean isDir) {
+        File file = new File(path);
+        if (!file.exists()) {
+            if (isDir) {
+                file.mkdirs();
+            } else {
+                try {
+                    file.createNewFile();
+                }
+                catch (IOException e) {
+                    log.error("创建文件失败：", e);
+                    return null;
+                }
+            }
+        }
+        return file;
+    }
+    
+    public static File createPath(String path){
+        return createPath(path, true);
+    }
+ 
+}
--- src/main/java/com/yuanfy/monitorsite/common/util/keystore/KeyStoreUtils.java
+++ src/main/java/com/yuanfy/monitorsite/common/util/keystore/KeyStoreUtils.java
@@ -1,11 +1,88 @@
 package com.yuanfy.monitorsite.common.util.keystore;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+import org.apache.log4j.Logger;
+
+import com.yuanfy.monitorsite.common.util.file.ResPathUtils;
+
+
 /**
- * @Description: 秘钥库工具类：用于生成私钥和公钥
+ * @Description: 密钥库工具类：用于生成私钥和公钥，可以作为一个单独的项目导出。
  * @author yuanfy
  * @date 2017年12月28日 下午7:43:46 
  * @version 1.0
  */
 public class KeyStoreUtils {
-
+    
+    private static Logger log = Logger.getLogger(KeyStoreUtils.class);
+    /**
+     * 设置别名，默认为mykey
+     */
+    private final static String ALIAS = "blog1.0.0";
+    /**
+     * 密钥库密码
+     */
+    private final static String KEY_PASS = "blogpassw0rd520";
+    /**
+     * 密钥密码
+     */
+    private final static String STORAGE_PASS = "blogpassw0rd520";
+    /**
+     * 存放密钥对文件
+     */
+    private final static String STORAGE_FILE = "blog.jks";
+    
+    public static void generateKeyPair() {
+        String filePath = ResPathUtils.getKeyStorePath() + ResPathUtils.fileSeparator + STORAGE_FILE;
+        String command = createCommand(filePath);
+        Process process = null;
+        try {
+            if (ResPathUtils.isWindows()) {
+                process = Runtime.getRuntime().exec("cmd /c " + command);
+            } else {
+                String[] cmds = {"/bin/sh", "-c", command};
+                process = Runtime.getRuntime().exec(cmds);
+            }
+            process.waitFor();//等待执行完毕
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream(), Charset.forName("gb2312")));
+            String line = null;
+            StringBuffer msg = new StringBuffer();
+            boolean isError = false;
+            while ((line = reader.readLine()) != null) {
+                msg.append(line);
+                if (line.contains("错误") || line.contains("error")) {
+                    isError = true;
+                }
+            }
+            if (isError) {
+                log.error("密钥库生成失败:" + msg);
+            } else {
+                log.info("密钥库生成成功:" + msg);
+            }
+        }
+        catch (IOException e) {
+            log.error("密钥库生成失败，命令执行有问题：", e);
+        }
+        catch (InterruptedException e) {
+            log.error("密钥库生成失败，命令执行线程中断异常：", e);
+        }
+    }
+    /**
+     * @Description: 创建密钥库命令
+     * @param filePath 存放密钥库文件路径
+     * @author yuanfy
+     * @date 2017年12月29日 下午4:31:45 
+     * @version 6.5
+     */
+    private static String createCommand(String filePath) {
+        StringBuffer command = new StringBuffer();
+        command.append("keytool -genkeypair -dname \"CN=YuanFY, OU=YuanFY, O=YuanFY, L=GZ, ST=GD, C=CN\" -alias ")
+        .append(ALIAS).append(" -keyalg RSA -keystore ").append(filePath).append(" -keypass ")
+        .append(KEY_PASS).append(" -storepass ").append(STORAGE_PASS).append(" -validity 100000");
+        return command.toString();
+    }
 }
